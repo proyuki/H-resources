3b8bfc30ab73d460f3e0d1502abfd3f627496291
proyuki <programmeryuki0218@gmail.com>
2020-12-17 16:50:53 +0900

    Create layouts-admin-right_column

---
+++ resources/views/layouts/admin/right_column.blade.php
@@ -0,0 +1,22 @@
+<section class="staff">
+  <h3 class="staff-top">スタッフ一覧</h3>
+  <table class="staff-lists">
+    <tr class="staff-list">
+      <td class="staff-area" rowspan="3">家電量販店A</td>
+      <td class="staff-name">山田 太郎さん</td>
+    </tr>
+    <tr class="staff-list">
+      <td class="staff-name">佐藤 太郎さん</td>
+    </tr>
+    <tr class="staff-list">
+      <td class="staff-name">吉田 太郎さん</td>
+    </tr>
+    <tr class="staff-list">
+      <td class="staff-area" rowspan="2">家電量販店B</td>
+      <td class="staff-name">山田 太郎さん</td>
+    </tr>
+    <tr class="staff-list">
+      <td class="staff-name">佐藤 太郎さん</td>
+    </tr>
+  </table>
+</section>
