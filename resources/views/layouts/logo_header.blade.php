b2acb132b8927cee70e3fb6afe66d3907f1d51c0
proyuki <programmeryuki0218@gmail.com>
2020-12-17 15:21:35 +0900

    Welcome page file split

---
+++ resources/views/layouts/logo_header.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  {{-- CSRF Token --}}
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <title>
+    @yield('title')
+  </title>
+
+  {{-- Scripts --}}
+  <script src="{{ asset('js/app.js') }}" defer></script>
+
+  {{-- Style --}}
+  <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+
+<body>
+
+  <header class="logo-header">
+    <div class="logo-container">
+      <img src="{{ asset('image/logo.png') }}" alt="ロゴ画像">
+    </div>
+  </header>
+
+  @yield('content')
+
+</body>
+
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,103 +1,85 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.logo_header')
 
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+@section('title', 'H-resources')
 
-  <title>H-resources</title>
+@section('content')
 
-  {{-- Style --}}
-  <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
-  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-</head>
+<div class="user-container">
 
-<body>
-
-  <header class="logo-header">
-    <div class="logo-container">
-      <img src="{{ asset('image/logo.png') }}" alt="ロゴ画像">
+  <section class="top top-box">
+    <div class="top-left-box">
+      <h2 class="top-left-title">シフト提出・打刻・給料明細を<br>アプリで簡単管理</h2>
     </div>
-  </header>
 
-  <div class="user-container">
-
-    <section class="top top-box">
-      <div class="top-left-box">
-        <h2 class="top-left-title">シフト提出・打刻・給料明細を<br>アプリで簡単管理</h2>
+    <div class="top-right-box">
+      <div class="top-right-up-box">
+        <h3 class="top-right-title">新規登録はこちら</h3>
+        <button class="button-link"><a href="">新規登録</a></button>
       </div>
+      <div class="top-right-down-box">
+        <h3 class="top-right-title">ログインはこちら</h3>
+        <div class="top-right-down-button-box">
+          <button class="button-link"><a href="">スタッフ様</a></button>
+          <button class="button-link"><a href="">管理者様</a></button>
+        </div>
+      </div>
+    </div>
+  </section>
 
-      <div class="top-right-box">
-        <div class="top-right-up-box">
-          <h3 class="top-right-title">新規登録はこちら</h3>
-          <button class="button-link"><a href="">新規登録</a></button>
-        </div>
-        <div class="top-right-down-box">
-          <h3 class="top-right-title">ログインはこちら</h3>
-          <div class="top-right-down-button-box">
-            <button class="button-link"><a href="">スタッフ様</a></button>
-            <button class="button-link"><a href="">管理者様</a></button>
-          </div>
-        </div>
+  <div class="content">
+
+    <section class="top content-box">
+      <div class="content-image-box content-left-box">
+        <img class="content-image" src="{{ asset('image/shift.png') }}" alt="シフト管理の画面">
+      </div>
+      <div class="content-text-box content-right-box">
+        <p class="content-text">
+          一目でシフトの確認ができる。<br>
+          直感的な操作でシフト情報を編集。<br>
+          スタッフへの共有もボタン一つ。
+        </p>
       </div>
     </section>
 
-    <div class="content">
+    <section class="top content-box">
+      <div class="content-text-box content-left-box">
+        <p class="content-text">
+          出退勤の管理を一画面で確認。<br>
+          勤怠の見える化を実現。
+        </p>
+      </div>
+      <div class="content-image-box content-right-box">
+        <img class="content-image" src="{{ asset('image/stamping.png') }}" alt="打刻管理の画面">
+      </div>
+    </section>
 
-      <section class="top content-box">
-        <div class="content-image-box content-left-box">
-          <img class="content-image" src="{{ asset('image/shift.png') }}" alt="シフト管理の画面">
-        </div>
-        <div class="content-text-box content-right-box">
-          <p class="content-text">
-            一目でシフトの確認ができる。<br>
-            直感的な操作でシフト情報を編集。<br>
-            スタッフへの共有もボタン一つ。
-          </p>
-        </div>
-      </section>
-
-      <section class="top content-box">
-        <div class="content-text-box content-left-box">
-          <p class="content-text">
-            出退勤の管理を一画面で確認。<br>
-            勤怠の見える化を実現。
-          </p>
-        </div>
-        <div class="content-image-box content-right-box">
-          <img class="content-image" src="{{ asset('image/stamping.png') }}" alt="打刻管理の画面">
-        </div>
-      </section>
-
-      <section class="top content-box">
-        <div class="content-image-box content-left-box">
-          <img class="content-image" src="{{ asset('image/salary.png') }}" alt="給与管理の画面">
-        </div>
-        <div class="content-text-box content-right-box">
-          <p class="content-text">
-            スタッフ別の給料管理はもちろん、<br>
-            法人全体の支払い情報も。
-          </p>
-        </div>
-      </section>
-
-    </div>
-
-    <section class="top bottom-box">
-      <p class="bottom-box-text">
-        「シフト管理」「打刻管理」「給料管理」<br>
-        ３つの機能を１つのアプリに集約。<br>
-        業務の効率化はこちらから。
-      </p>
-      <button class="button-link"><a href="">新規登録</a></button>
+    <section class="top content-box">
+      <div class="content-image-box content-left-box">
+        <img class="content-image" src="{{ asset('image/salary.png') }}" alt="給与管理の画面">
+      </div>
+      <div class="content-text-box content-right-box">
+        <p class="content-text">
+          スタッフ別の給料管理はもちろん、<br>
+          法人全体の支払い情報も。
+        </p>
+      </div>
     </section>
 
   </div>
 
-  <footer class="footer">
-    <p class="footer-text">© Copyright2020 H-resources All Rights Reserved.</p>
-  </footer>
+  <section class="top bottom-box">
+    <p class="bottom-box-text">
+      「シフト管理」「打刻管理」「給料管理」<br>
+      ３つの機能を１つのアプリに集約。<br>
+      業務の効率化はこちらから。
+    </p>
+    <button class="button-link"><a href="">新規登録</a></button>
+  </section>
 
-</body>
+</div>
 
-</html>
+<footer class="footer">
+  <p class="footer-text">© Copyright2020 H-resources All Rights Reserved.</p>
+</footer>
+
+@endsection
