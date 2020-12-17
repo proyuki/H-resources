0403b5fb36824a16d29ea4a486059c7f5bf1174d
proyuki <programmeryuki0218@gmail.com>
2020-12-17 13:14:00 +0900

    Change default redirect destination

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,8 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/user/home';
+    public const ADMIN_HOME = '/admin/home';
 
     /**
      * Define your route model bindings, pattern filters, etc.
